ece38a005fb50477bc85a7f2bc40cb0e31cdf78a
difnif <openspace1003@gmail.com>
2026-04-22 16:04:16 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { ActionProvider } from './contexts/ActionContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <ThemeProvider>
-          <App />
+          <ActionProvider>
+            <App />
+          </ActionProvider>
         </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
